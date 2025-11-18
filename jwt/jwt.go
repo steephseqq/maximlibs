706bc59ef493b3486b898041ea913ff624056e30
steephseqq <steephseqq@proton.me>
2025-11-18 07:44:32 -0500

    change NewToken parameters from user.Field to string

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -5,17 +5,16 @@ import (
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
-	"github.com/steephseqq/maximlibs/domains/models"
 )
 
 var secret = os.Getenv("JWT_SECRET")
 
-func NewToken(user models.User, duration time.Duration) (string, error) {
+func NewToken(userUUID, userEmail string, duration time.Duration) (string, error) {
 	token := jwt.New(jwt.SigningMethodHS256)
 
 	claims := token.Claims.(jwt.MapClaims)
-	claims["uid"] = user.UUID
-	claims["email"] = user.Email
+	claims["uid"] = userUUID
+	claims["email"] = userEmail
 	claims["exp"] = time.Now().Add(duration).Unix()
 
 	tokenString, err := token.SignedString([]byte(secret))
