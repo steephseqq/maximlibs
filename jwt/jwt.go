a8b1bc3ae6dbb85bc9a869684007c880625dfb31
steephseqq <steephseqq@proton.me>
2025-11-23 10:46:40 -0500

    add GetUserID & ParseToken func's

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -1,13 +1,29 @@
 package jwt
 
 import (
+	"context"
+	"errors"
 	"os"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 )
 
-var secret = os.Getenv("JWT_SECRET")
+var (
+	secret          = os.Getenv("JWT_SECRET")
+	ErrInvalidToken = errors.New("invalid token")
+)
+
+type contextKey string
+
+const (
+	UserIDKey contextKey = "userID"
+)
+
+type Claims struct {
+	UserID string `json:"uid"`
+	Exp    int64  `json:"exp"`
+}
 
 func NewToken(userUUID, userEmail string, duration time.Duration) (string, error) {
 	token := jwt.New(jwt.SigningMethodHS256)
@@ -23,4 +39,37 @@ func NewToken(userUUID, userEmail string, duration time.Duration) (string, error
 	}
 
 	return tokenString, nil
+}
+
+func GetUserID(ctx context.Context) (string, bool) {
+	userID, ok := ctx.Value(UserIDKey).(string)
+	return userID, ok
+}
+
+func ParseToken(tokenString, secret string) (*Claims, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return []byte(secret), nil
+	})
+
+	if err != nil || !token.Valid {
+		return nil, ErrInvalidToken
+	}
+
+	claims, _ := token.Claims.(jwt.MapClaims)
+	exp, ok := claims["exp"].(float64)
+	if !ok {
+		return nil, ErrInvalidToken
+	}
+	if time.Now().Unix() > int64(exp) {
+		return nil, ErrInvalidToken
+	}
+
+	uID, ok := claims["uid"].(string)
+	if !ok {
+		return nil, ErrInvalidToken
+	}
+	return &Claims{
+		UserID: uID,
+		Exp:    int64(exp),
+	}, nil
 }
